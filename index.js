bde99291ce8106d9cfb33d646578146e0b4f8319
Jorge Sosa Almeida <dorwinrin@gmail.com>
2022-07-24 08:42:45 +0200

    added js tiles

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const tilesAmount = 16;
-const toggledTiles = new Array(16).fill(false);
+const FLAG = '🚩';
+const BOMB = '💣';
+const tilesAmount = 96;
+const toggledTiles = new Array(tilesAmount).fill(false);
+const flaggedTiles = new Array(tilesAmount).fill(false);
 
 (function init() {
   console.log('Init game');
@@ -7,20 +10,41 @@ const toggledTiles = new Array(16).fill(false);
 })();
 
 function initTiles(tiles) {
+  const board = document.getElementById('board');
   for (let i = 1; i <= tiles; i++) {
-    const tile = document.getElementById(`casilla-${i}`);
-    tile.addEventListener('click', handleClick(tile, i));
+    const tile = document.createElement('div');
+    tile.setAttribute('id', `tile-${i}`);
+    tile.classList.add('tile');
+
+    tile.onclick = handleClick(tile, i);
+    tile.oncontextmenu = handleRightClick(tile, i);
+    board.appendChild(tile);
   }
 }
 
 function handleClick(tile, index) {
   return () => {
     if (toggledTiles[index]) {
-      tile.classList.remove('clicked');
+      tile.classList.remove('revealed');
       toggledTiles[index] = false;
     } else {
-      tile.classList.add('clicked');
+      tile.classList.add('revealed');
       toggledTiles[index] = true;
+    }
+  };
+}
+
+function handleRightClick(tile, index) {
+  return (event) => {
+    event.preventDefault();
+    if (flaggedTiles[index]) {
+      tile.classList.remove('flagged');
+      tile.innerText = '';
+      flaggedTiles[index] = false;
+    } else {
+      tile.classList.add('flagged');
+      tile.innerText = FLAG;
+      flaggedTiles[index] = true;
     }
   };
 }
